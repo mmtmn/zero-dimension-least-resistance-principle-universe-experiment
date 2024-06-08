6d88d82bbc28d6d859806db12fb98ebd583cbc2a
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2024-06-07 23:39:00 -0600

    Update postquantum-theory-of-classical-gravity.c

--- postquantum-theory-of-classical-gravity.c
+++ postquantum-theory-of-classical-gravity.c
@@ -11,15 +11,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define NUM_QUANTUM_SYSTEMS 1000
+#define NUM_QUANTUM_SYSTEMS 10000
 #define G 0.001f
 #define TIME_STEP 0.01f
 #define SPACETIME_FLUCTUATION_SCALE 1e-5f
+#define DECOHERENCE_RATE 0.01f
 
 typedef struct {
     float x, y, z;
     float vx, vy, vz;
     bool isQuantum;
+    float coherence;
 } System;
 
 float cameraX = 0.0f, cameraY = 0.0f, cameraZ = 50.0f;
@@ -38,9 +40,9 @@ void drawPoint(System s) {
     glPushMatrix();
     glTranslatef(s.x, s.y, s.z);
     if (s.isQuantum) {
-        glColor3f(0.0, 1.0, 0.0); // Green for quantum systems
+        glColor3f(0.0, 1.0, 0.0);
     } else {
-        glColor3f(1.0, 0.0, 0.0); // Red for classical systems
+        glColor3f(1.0, 0.0, 0.0);
     }
     glutSolidSphere(0.1, 10, 10);
     glPopMatrix();
@@ -58,8 +60,9 @@ void initializeSystems(void) {
         float vz = ((float)rand() / RAND_MAX - 0.5) * 0.1f;
 
         bool isQuantum = (rand() % 2) == 0;
+        float coherence = 1.0f;
 
-        System s = {x, y, z, vx, vy, vz, isQuantum};
+        System s = {x, y, z, vx, vy, vz, isQuantum, coherence};
         systems[numSystems++] = s;
     }
 }
@@ -71,6 +74,47 @@ void applySpacetimeFluctuations(System* systems, int numSystems) {
             systems[i].x += ((float)rand() / RAND_MAX - 0.5) * SPACETIME_FLUCTUATION_SCALE;
             systems[i].y += ((float)rand() / RAND_MAX - 0.5) * SPACETIME_FLUCTUATION_SCALE;
             systems[i].z += ((float)rand() / RAND_MAX - 0.5) * SPACETIME_FLUCTUATION_SCALE;
+        }
+    }
+}
+
+void applyGravitationalInteraction(System* systems, int numSystems) {
+    #pragma omp parallel for
+    for (int i = 0; i < numSystems; i++) {
+        float fx = 0.0f;
+        float fy = 0.0f;
+        float fz = 0.0f;
+
+        for (int j = 0; j < numSystems; j++) {
+            if (i != j) {
+                float dx = systems[j].x - systems[i].x;
+                float dy = systems[j].y - systems[i].y;
+                float dz = systems[j].z - systems[i].z;
+                float distance = sqrt(dx * dx + dy * dy + dz * dz);
+                if (distance > 0.01f) {
+                    float force = (G / (distance * distance)) * (1.0f / distance);
+                    fx += force * dx;
+                    fy += force * dy;
+                    fz += force * dz;
+                }
+            }
+        }
+
+        systems[i].vx += fx * TIME_STEP;
+        systems[i].vy += fy * TIME_STEP;
+        systems[i].vz += fz * TIME_STEP;
+    }
+}
+
+void applyDecoherence(System* systems, int numSystems) {
+    #pragma omp parallel for
+    for (int i = 0; i < numSystems; i++) {
+        if (systems[i].isQuantum) {
+            systems[i].coherence -= DECOHERENCE_RATE * TIME_STEP;
+            if (systems[i].coherence <= 0.0f) {
+                systems[i].isQuantum = false;
+                systems[i].coherence = 0.0f;
+            }
         }
     }
 }
@@ -102,6 +146,8 @@ void display(void) {
               0.0, 1.0, 0.0);
 
     applySpacetimeFluctuations(systems, numSystems);
+    applyGravitationalInteraction(systems, numSystems);
+    applyDecoherence(systems, numSystems);
     updateSystems(systems, numSystems);
 
     for (int i = 0; i < numSystems; i++) {
@@ -189,4 +235,3 @@ int main(int argc, char **argv) {
     glutMainLoop();
     return 0;
 }
-
